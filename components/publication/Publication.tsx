c866397924161c81d8a5dec4331e46a567813ffe
Nathan Gomes <nahtann@outlook.com>
2022-03-03 21:01:49 -0300

    writing data on contract

--- components/publication/Publication.tsx
+++ components/publication/Publication.tsx
@@ -1,4 +1,14 @@
-import { Avatar, Card, Text, Tooltip } from '@nextui-org/react';
+import {
+  Avatar,
+  Button,
+  Card,
+  Input,
+  Loading,
+  Modal,
+  Text,
+  Textarea,
+  Tooltip,
+} from '@nextui-org/react';
 import { ethers, Contract } from 'ethers';
 import { useContext, useEffect, useState } from 'react';
 import { AiFillStar, AiOutlineStar } from 'react-icons/ai';
@@ -13,13 +23,41 @@ const Publication = ({ cardData }: { cardData: CardDataType }) => {
   const { currentAccount, getSigner } = useContext(CheckWalletContext);
   const [starred, setStarred] = useState(false);
   const [stars, setStars] = useState(0);
+  const [addStarVisible, setAddStarVisible] = useState(false);
+  const [removeStarVisible, setRemoveStarVisible] = useState(false);
+  const [message, setMessage] = useState('');
+  const [addStarLoading, setAddStarLoading] = useState(false);
 
   const bodyText = cardData.description.split('<links>');
 
-  const handleClick = () => {
-    if (currentAccount !== undefined) {
-      return setStarred(!starred);
-    }
+  const handleClickOpenOutlineStar = () => {
+    setAddStarVisible(true);
+  };
+  const handleClickCloseOutlineStar = () => setAddStarVisible(false);
+  const sendStar = async () => {
+    let signer = await getSigner();
+
+    const starProject = new ethers.Contract(
+      cardData.contract,
+      enStarProject.abi,
+      signer
+    );
+
+    const starTxn = await starProject.star(message);
+    setAddStarLoading(true);
+    await starTxn.wait();
+
+    const count = await starProject.getTotalStars();
+    setStars(count.toNumber());
+
+    setAddStarLoading(false);
+    return handleClickCloseOutlineStar();
+  };
+
+  const closeRemoveStarModal = () => setRemoveStarVisible(false);
+
+  const removeStar = () => {
+    setRemoveStarVisible(true);
   };
 
   const getTotalStars = async () => {
@@ -33,6 +71,28 @@ const Publication = ({ cardData }: { cardData: CardDataType }) => {
 
     const count = await starProject.getTotalStars();
     setStars(count.toNumber());
+  };
+
+  const getStarButtons = () => {
+    if (starred) {
+      return (
+        <AiFillStar
+          size={27}
+          color={'#f5a623'}
+          className={styles.card__header__star}
+          onClick={removeStar}
+        />
+      );
+    }
+
+    return (
+      <AiOutlineStar
+        size={27}
+        color={'#f5a623'}
+        className={styles.card__header__star}
+        onClick={handleClickOpenOutlineStar}
+      />
+    );
   };
 
   useEffect(() => {
@@ -52,23 +112,85 @@ const Publication = ({ cardData }: { cardData: CardDataType }) => {
             <Text h4>{cardData.title}</Text>
           </div>
 
-          {starred ? (
-            <AiFillStar
-              size={27}
-              color={'#f5a623'}
-              className={styles.card__header__star}
-              onClick={handleClick}
-            />
-          ) : (
+          {currentAccount === undefined ? (
             <Tooltip content={cardData.tooltip} trigger="click">
               <AiOutlineStar
                 size={27}
                 color={'#f5a623'}
                 className={styles.card__header__star}
-                onClick={handleClick}
               />
             </Tooltip>
+          ) : (
+            getStarButtons()
           )}
+
+          {/* Add Star Modal */}
+          <Modal
+            preventClose
+            closeButton
+            blur
+            open={addStarVisible}
+            onClose={handleClickCloseOutlineStar}
+          >
+            <Modal.Header>
+              <Text h2>Add Star</Text>
+            </Modal.Header>
+            <Modal.Body>
+              <Input disabled value={currentAccount} label="Account" />
+              <Input
+                disabled
+                value={`${cardData.title} - ${cardData.lang}`}
+                label="Project"
+              />
+              <Textarea
+                label="Message"
+                bordered
+                rows={3}
+                maxRows={3}
+                onChange={e => setMessage(e.target.value)}
+                placeholder="Hello World..."
+              />
+            </Modal.Body>
+            <Modal.Footer>
+              <Button
+                auto
+                flat
+                color={'error'}
+                onClick={handleClickCloseOutlineStar}
+              >
+                Close
+              </Button>
+              <Button auto onClick={sendStar} shadow>
+                {addStarLoading ? (
+                  <Loading color={'white'} size={'sm'} type={'points'} />
+                ) : (
+                  'Send Star'
+                )}
+              </Button>
+            </Modal.Footer>
+          </Modal>
+
+          {/* Remove Star Modal */}
+          <Modal
+            preventClose
+            closeButton
+            blur
+            open={removeStarVisible}
+            onClose={closeRemoveStarModal}
+          >
+            <Modal.Header>
+              <Text h3>Add Star</Text>
+            </Modal.Header>
+            <Modal.Body>Estrela</Modal.Body>
+            <Modal.Footer>
+              <Button auto flat color={'error'} onClick={closeRemoveStarModal}>
+                Close
+              </Button>
+              <Button auto onClick={closeRemoveStarModal}>
+                Send Star
+              </Button>
+            </Modal.Footer>
+          </Modal>
         </Card.Header>
         <Card.Body>
           <Text key={cardData.title}>
